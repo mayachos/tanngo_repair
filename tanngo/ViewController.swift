9bcfea22b3b398b53b8475282b7e68ba109cc0c0
ena <ey717108@icloud.com>
2021-05-30 11:40:23 +0900

    Initial Commit

---
+++ tanngo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tanngo
+//
+//  Created by 山邉瑛愛 on 2021/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
